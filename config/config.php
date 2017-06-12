9b31daf261d588e62700b73997f5afd3ea9ed3ec
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2017-06-12 19:19:02 -0300

    trocada config para conectar com mysql

--- config/config.php
+++ config/config.php
@@ -1,10 +1,18 @@
 <?php
 
 return array(
+//    'database' => array(
+//        'driver' => 'pdo_pgsql',
+//        'dbname' => 'postgres',
+//        'host' => 'postgres',
+//        'user' => 'postgres',
+//        'password' => 'abcd1234',
+//    )
     'database' => array(
-        'dbname' => 'postgres',
-        'host' => 'postgres',
-        'user' => 'postgres',
-        'password' => 'abcd1234',
+        'driver' => 'pdo_mysql',
+        'dbname' => 'n2oti',
+        'host' => 'mysql',
+        'user' => 'n2oti',
+        'password' => 'n2oti',
     )
 );
