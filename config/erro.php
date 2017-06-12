6e91b735b2d3c0d8a80bb7f395d7b31792dd7040
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2017-06-12 14:58:23 -0300

    simplicficado para capturar apenas erros de domain exception

--- config/erro.php
+++ config/erro.php
@@ -4,23 +4,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app->error(function (Exception $exception, Request $request, $code) use ($app) {
-    switch ($code) {
-        case Response::HTTP_NOT_FOUND:
-            $message = 'The requested page could not be found.';
-            break;
-        case Response::HTTP_INTERNAL_SERVER_ERROR:
-            if ($exception instanceof DomainException) {
-                $message = $exception->getMessage();
-                $code = Response::HTTP_BAD_REQUEST;
-                break;
-            }
-            if (true === $app['debug']) {
-                return;
-            }
-        default:
-            $message = "We are sorry, but something went terribly wrong: {$exception->getMessage()}";
-            break;
-    }
-    return new JsonResponse(array('errors'=> explode('|', $message)), $code);
+$app->error(function (DomainException $exception, Request $request, $code) use ($app) {
+    return new JsonResponse(array('errors'=> explode('|', $exception->getMessage())), Response::HTTP_BAD_REQUEST);
 });
